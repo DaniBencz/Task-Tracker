f7fccff1ab4be1afdf1c60a6ad2553baf5a83c43
Daniel Bencz <daniel.bencz@gmail.com>
2021-11-25 14:08:37 +0100

    httpClient

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
-import { TASKS } from '../mock-tasks';
+// import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks';
 
-  constructor() { }
+  // invoke service in the constructor
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
     // turning TASKS into an observable
-    return of(TASKS);
+    // return of(TASKS);
+
+    // HttpClientModule also needs to be added to imports in app.module!
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
