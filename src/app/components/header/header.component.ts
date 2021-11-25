888c8757434423365fbebc15aa6d23081d1b27db
Daniel Bencz <daniel.bencz@gmail.com>
2021-11-25 14:35:43 +0100

    delete task handler

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   title = 'Task Tracker';
   toggleAddTask() {
-    console.log('foo');
+    console.log('add');
   }
 
   constructor() { }
